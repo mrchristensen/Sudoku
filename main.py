97e45906194d35d7b3b7440418131cc122fa4909
Matt <mr.christensen@gmail.com>
2021-10-13 20:46:22 -0600

    Implemented concretization of a random value and checking for solved board/square

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 
 
@@ -7,6 +8,25 @@ class Square:
 
     def __str__(self):
         return str(self.array)
+
+    # Concretize a random value in this square
+    def concretize_random_value(self):
+        while True:
+            i, j = random.randint(0, 2), random.randint(0, 2)
+
+            if len(self.array[i][j]) == 1:  # if the set possible values is just 1 then find another value
+                continue
+            else:
+                # Pick a random value from the set of possibilities
+                self.array[i][j] = random.choice(tuple(self.array[i][j]))
+                return i, j
+
+    # Check to see if this square is solved
+    def solved(self):
+        for value in self.array.flatten():
+            if len(value) != 1:
+                return False
+        return True
 
 
 class Game:
@@ -38,11 +58,38 @@ class Game:
 
         return ret_string
 
+    # Concretize a random value in the board
+    def concretize_random_value(self):
+        while True:
+            random_index = random.randint(0, 8)
+
+            if self.board.flatten()[random_index].solved():
+                continue
+
+            i, j = self.board.flatten()[random_index].concretize_random_value()
+            break
+
+        self.update_board(i, j)
+
+    # Check to see if the game board is completely solved
+    def solved(self):
+        for square in self.board.flatten():  # Check all the square to see if there are solved
+            if square.solved() is False:
+                return False
+
+        return True
+
+    # Update the lists of possible values based off a decision
+    def update_board(self, i, j):
+        pass
+
 
 def print_hi(name):
     my_game = Game()
     print(my_game)
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    my_game.concretize_random_value()
+    print(my_game)
+    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 if __name__ == '__main__':
